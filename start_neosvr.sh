6c394452ebd9b6273b2603b263d8e1f7af4ba4cc
Shadow Panther <panther@manul.static.corbina.ru>
2021-04-15 17:20:09 +0300

    Cleanup startup file a bit

--- start_neosvr.sh
+++ start_neosvr.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-bash "${STEAMCMDDIR}/steamcmd.sh" +login ${STEAMLOGIN} +force_install_dir ${STEAMAPPDIR} +app_license_request ${STEAMAPPID} \
-	"+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD}" validate +quit
+bash "${STEAMCMDDIR}/steamcmd.sh" \
+	+login ${STEAMLOGIN} \
+	+force_install_dir ${STEAMAPPDIR} \
+	+app_license_request ${STEAMAPPID} \
+	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
+	+quit
 find ./Data/Assets -atime +7 -delete
 find ./Data/Cache -atime +7 -delete
 find /Logs -atime +30 -delete
