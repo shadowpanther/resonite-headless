a4e4ab1af7a2bf9ee37d5d712482b4929eec40aa
Shadow Panther <panther.ru@gmail.com>
2022-01-26 23:32:06 +0300

    Fix: Please use force_install_dir before logon!
    Please use force_install_dir before logon!
    
    https://github.com/ValveSoftware/steam-for-linux/issues/8298

--- start_neosvr.sh
+++ start_neosvr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 bash "${STEAMCMDDIR}/steamcmd.sh" \
-	+login ${STEAMLOGIN} \
 	+force_install_dir ${STEAMAPPDIR} \
+	+login ${STEAMLOGIN} \
 	+app_license_request ${STEAMAPPID} \
 	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
 	+quit
