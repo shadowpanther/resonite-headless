5653ddee76a427e2bc4c6152b73172c5858558c6
Shadow Panther <panther.ru@gmail.com>
2024-08-06 14:04:48 +0200

    Remove checking for the temporary binary location from the start script.

--- src/start_resonite.sh
+++ src/start_resonite.sh
@@ -3,9 +3,6 @@
 if [ -f ${STEAMAPPDIR}/Headless/Resonite.dll ]; then
 	echo 'Resonite.dll is in the new (permanent) location, running...'
 	exec dotnet ${STEAMAPPDIR}/Headless/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs
-elif [ -f ${STEAMAPPDIR}/Headless/net8.0/Resonite.dll ]; then
-	echo 'Resonite.dll is in the old (temporary) location, running...'
-	exec dotnet ${STEAMAPPDIR}/Headless/net8.0/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs
 else
 	echo 'Resonite.dll not found, weird!'
 	sleep 10
