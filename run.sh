6924a594c82b9c358d97daa9464165500370c509
Shadow Panther <panther.ru@gmail.com>
2024-08-06 14:02:27 +0200

    Make `run.sh` to check step-by-step whether we have the important configs.

--- run.sh
+++ run.sh
@@ -3,9 +3,7 @@
 if [ ! -f ./Config/Config.json ]; then
     cp ./Config/DefaultConfig.json ./Config/Config.json
     echo "Created a Congig.json in the ./Config folder for you. Please edit it accordingly."
-fi
-
-if [ ! -f ./docker-compose.yaml ]; then
+elif [ ! -f ./docker-compose.yaml ]; then
     cp ./docker-compose.sample.yaml ./docker-compose.yaml
     echo "Created a docker-compose.yaml in the current folder for you. Please fill in the needed parameters."
 else
