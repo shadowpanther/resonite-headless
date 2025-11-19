5dbf1cd446c57d290086a3e1feae2483f3cffc56
Baplar <baplar@gmail.com>
2025-11-19 14:26:53 +0100

    Use dotnet-install to setup local .NET runtime

--- src/setup_resonite.sh
+++ src/setup_resonite.sh
@@ -7,6 +7,10 @@ bash "${STEAMCMDDIR}/steamcmd.sh" \
 	+app_license_request ${STEAMAPPID} \
 	+app_update ${STEAMAPPID} -beta ${STEAMBETA} -betapassword ${STEAMBETAPASSWORD} validate \
 	+quit
+
+chmod +x ${STEAMAPPDIR}/dotnet-install.sh
+${STEAMAPPDIR}/dotnet-install.sh --channel ${DOTNETVERSION} --runtime dotnet --install-dir ${STEAMAPPDIR}/dotnet-runtime
+
 find ${STEAMAPPDIR}/Data/Assets -type f -atime +7 -delete
 find ${STEAMAPPDIR}/Data/Cache -type f -atime +7 -delete
 find /Logs -type f -name *.log -atime +30 -delete
--- src/start_resonite.sh
+++ src/start_resonite.sh
@@ -2,7 +2,7 @@
 
 if [ -f ${STEAMAPPDIR}/Headless/Resonite.dll ]; then
 	echo 'Resonite.dll is in the new (permanent) location, running...'
-	exec dotnet ${STEAMAPPDIR}/Headless/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs
+	exec ${STEAMAPPDIR}/dotnet-runtime/dotnet ${STEAMAPPDIR}/Headless/Resonite.dll -HeadlessConfig /Config/Config.json -Logs /Logs
 else
 	echo 'Resonite.dll not found, weird!'
 	sleep 10
